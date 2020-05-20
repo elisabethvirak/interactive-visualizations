8323280c591d723bc9a5aca4fb735186a3f68e26
Elisabeth <elisabethvirak@gmail.com>
2020-05-20 16:23:02 -0400

    Began buildplot

--- js/app.js
+++ js/app.js
@@ -6,22 +6,9 @@ function init () {
         // sanity check
         // console.log(data);
 
-        // collect variables to be used later on
-        // save metadata to be used in demographic info
-        var metadata = data.metadata[0];
-        // console.log(metadata);
         // save subject ids as variable
         var subjectIDs = data.names;
         // console.log(subjectIDs);
-        // save OTU IDs as a variable
-        var otuIDs = data.samples[0].otu_ids;
-        // console.log(otuIDs);
-        // save sample values as a variable
-        var sampleValues = data.samples[0].sample_values;
-        // console.log(sampleValues);
-        // save OTU labels as a variable
-        var otuLabels = data.samples[0].otu_labels;
-        // console.log(otuLabels);
 
         // select dropdown menu
         var dropdownMenu = d3.select('#selDataset');
@@ -31,13 +18,29 @@ function init () {
             var menuOption = dropdownMenu.append('option');
             menuOption.text(i).property('value');
         });
-
-        // clear webpage so no option is selected
-        // dropdownMenu.html("");
-
-        // datanames.forEach((name) =>
-        //     selector.append('option'),text(name).property('value',name))
+        buildPlot(subjectIDs[0]);
     });
-};
+}
+
+function buildPlot(sample) {
+    //read in data file
+    const dataFile = "samples.json";
+    d3.json(dataFile).then(data => {
+        // console.log(sample);
+        // save metadata to be used in demographic info
+        var metadata = data.metadata[sample-940];
+        // console.log(metadata);
+        // save OTU IDs as a variable
+        var otuIDs = data.samples[sample-940].otu_ids;
+        console.log(otuIDs);
+        // save sample values as a variable
+        var sampleValues = data.samples[sample-940].sample_values;
+        console.log(sampleValues);
+        // save OTU labels as a variable
+        var otuLabels = data.samples[sample-940].otu_labels;
+        console.log(otuLabels);
+
+    });
+}
 
 init();
