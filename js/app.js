1889abcd0005725fd01125c15ab36f23131f80f0
Elisabeth <elisabethvirak@gmail.com>
2020-05-20 16:04:24 -0400

    Fixed Dropdown menu

--- js/app.js
+++ js/app.js
@@ -1,53 +1,43 @@
-// // Read in json file
-// function metaData(sample) {
-//     const dataFile = "samples.json";
-//     d3.json(dataFile).then(function(data) {
-//         console.log(data);
-//     });
-}
-// // Create outer function to change
-// function dropdownMenu(id) {
-//     // select id where the dropdown menu is located
-//     var dropdownMenu = d3.select('#selDataset');
-//     // clear data previously printed to page
-//     dropdownMenu.html("");
-//     // provide all IDs as options in the dropdown menu
-//     d3.json(samples).forEach(function(id) {
-//         var subjectID = id.names;
-//         dropdownMenu.append('p').text(subjectID);
-//     });
-// }
-
-function init (i) {
+// Initial function to set up page
+function init () {
     //read in data file
     const dataFile = "samples.json";
-    d3.json(dataFile).then(function(data) {
+    d3.json(dataFile).then(data => {
         // sanity check
-        console.log(data);
+        // console.log(data);
 
         // collect variables to be used later on
         // save metadata to be used in demographic info
-        var metadata = data.metadata[i];
+        var metadata = data.metadata[0];
+        // console.log(metadata);
         // save subject ids as variable
         var subjectIDs = data.names;
+        // console.log(subjectIDs);
         // save OTU IDs as a variable
-        var otuIDs = data.samples[i].otu_ids;
+        var otuIDs = data.samples[0].otu_ids;
+        // console.log(otuIDs);
         // save sample values as a variable
-        var sampleValues = data.samples[i].sample_values;
+        var sampleValues = data.samples[0].sample_values;
+        // console.log(sampleValues);
         // save OTU labels as a variable
-        var otuLabels = data.samples[i].otu_labels;
+        var otuLabels = data.samples[0].otu_labels;
+        // console.log(otuLabels);
 
         // select dropdown menu
         var dropdownMenu = d3.select('#selDataset');
         //iterate through subject id list to add choices to the dropdown menu
-        for (i in subjectIDs) {
-            var menuOption = dropdownMenu.append('option')
-            menuOption.text(subjectIDs[i]);
-        }
+        subjectIDs.forEach(i => {
+            // console.log(i);
+            var menuOption = dropdownMenu.append('option');
+            menuOption.text(i).property('value');
+        });
+
+        // clear webpage so no option is selected
+        // dropdownMenu.html("");
 
         // datanames.forEach((name) =>
         //     selector.append('option'),text(name).property('value',name))
     });
 };
 
-init(0);
+init();
